9e9f247c1f563fec259b2f73c522a94a9342f0e0
Emmanuel Rodríguez <rorem.94.developer@gmail.com>
2022-12-18 20:20:30 -0600

    feat: ♻️added validator layer
    Added zod dependency
    Added validator middleware
    Added publish endpoint

---
+++ src/validators/index.validator.ts
@@ -0,0 +1,17 @@
+import { PipeTransform, Injectable, BadRequestException } from '@nestjs/common'
+import { Schema } from 'zod'
+
+import { createPublishSchema } from '@validators/publish.validator'
+
+@Injectable()
+export class ZodValidationPipe implements PipeTransform {
+    constructor(private schema: Schema) {}
+
+    transform(value: any) {
+        const { success } = this.schema.safeParse(value)
+        if (!success) throw new BadRequestException('Validation failed')
+        return value
+    }
+}
+
+export { createPublishSchema }
